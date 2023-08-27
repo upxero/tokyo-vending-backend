a048ec5b14ddfcf2faa0ce020f04e69606b90d5f
Michael Bernard <104996295+Upxero@users.noreply.github.com>
2023-08-27 15:15:16 +0200

    first commit

---
+++ src/main/java/com/tokyovending/TokyoVending/TokyoVendingApplication.java
@@ -0,0 +1,13 @@
+package com.tokyovending.TokyoVending;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokyoVendingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokyoVendingApplication.class, args);
+	}
+
+}
