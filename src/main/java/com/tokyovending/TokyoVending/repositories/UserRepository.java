8e291434180c77802200b44b8e994efec69b9704
Michael Bernard <104996295+Upxero@users.noreply.github.com>
2023-09-07 06:52:23 +0200

    Added User and tested in Postman and pgAdmin

--- src/main/java/com/tokyovending/TokyoVending/repositories/UserRepository.java
+++ src/main/java/com/tokyovending/TokyoVending/repositories/UserRepository.java
@@ -1,4 +1,7 @@
 package com.tokyovending.TokyoVending.repositories;
 
-public interface UserRepository {
+import com.tokyovending.TokyoVending.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
--- src/main/java/com/tokyovending/TokyoVending/services/UserService.java
+++ src/main/java/com/tokyovending/TokyoVending/services/UserService.java
@@ -1,4 +1,47 @@
 package com.tokyovending.TokyoVending.services;
 
+import com.tokyovending.TokyoVending.models.User;
+import com.tokyovending.TokyoVending.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User updateUser(Long id, User user) {
+        User existingUser = userRepository.findById(id).orElse(null);
+        if (existingUser != null) {
+            existingUser.setUsername(user.getUsername());
+            existingUser.setPassword(user.getPassword());
+            existingUser.setEmail(user.getEmail());
+            existingUser.setProfilePicture(user.getProfilePicture());
+            return userRepository.save(existingUser);
+        }
+        return null;
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
 }
+
