c1e0509249a415e8fdd936024f366dcd0efb2b06
Michael Bernard <104996295+Upxero@users.noreply.github.com>
2023-09-12 16:26:06 +0200

    Added Product and tested in Postman and pgAdmin

--- src/main/java/com/tokyovending/TokyoVending/models/Product.java
+++ src/main/java/com/tokyovending/TokyoVending/models/Product.java
@@ -1,4 +1,54 @@
 package com.tokyovending.TokyoVending.models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "products")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Double price;
+
+    @Column(length = 255)
+    private String specifications;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getSpecifications() {
+        return specifications;
+    }
+
+    public void setSpecifications(String specifications) {
+        this.specifications = specifications;
+    }
 }
+
--- src/main/java/com/tokyovending/TokyoVending/services/ProductService.java
+++ src/main/java/com/tokyovending/TokyoVending/services/ProductService.java
@@ -1,4 +1,47 @@
 package com.tokyovending.TokyoVending.services;
 
+import com.tokyovending.TokyoVending.models.Product;
+import com.tokyovending.TokyoVending.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product getProductById(Long id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
+    public Product createProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    public Product updateProduct(Long id, Product product) {
+        Product existingProduct = productRepository.findById(id).orElse(null);
+        if (existingProduct != null) {
+            existingProduct.setName(product.getName());
+            existingProduct.setPrice(product.getPrice());
+            existingProduct.setSpecifications(product.getSpecifications());
+            return productRepository.save(existingProduct);
+        }
+        return null;
+    }
+
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
+    }
 }
+
